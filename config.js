89bb956b2fceb1b2134934e3d1579e5da4e08117
Ironpurush0 <ridamsaxena90.com>
2020-12-01 13:23:20 +0530

    adding the firebase coonfig.

---
+++ config.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+export default config = {
+    apiKey: "AIzaSyBV41S3q4iUPGQi8WEoj387Ae-OOmv6Eds",
+    authDomain: "instagram-dev-80dfa.firebaseapp.com",
+    databaseURL: "https://instagram-dev-80dfa.firebaseio.com",
+    projectId: "instagram-dev-80dfa",
+    storageBucket: "instagram-dev-80dfa.appspot.com",
+    messagingSenderId: "1006436175262",
+    appId: "1:1006436175262:web:cf9342709a07f1095c9a8f",
+    measurementId: "G-XY2VGKGP0K"
+}
+
+firebase.initializeApp(config)
